3805e4802d85e0d2d1353bc6f00549d033c31c89
ChromatisGit <christian.loginmail@gmail.com>
2026-03-19 07:30:14 +0100

    fix: stop pdf build in prod

--- pipeline/main.ts
+++ pipeline/main.ts
@@ -6,19 +6,25 @@ import { getTopicLabels, resolveCourses } from "./dataTransformer/resolveCourses
 import { cleanImageOutput, cleanPdfOutput, compilePdfToPublic, deleteGenerated } from "./io";
 
 export async function runPipeline() {
+    const skipPdf = process.env.NODE_ENV === "production";
+
     await deleteGenerated();
     await cleanImageOutput();
-    await cleanPdfOutput();
+    if (!skipPdf) await cleanPdfOutput();
     const coursePlans = await loadConfigs()
     const pagePaths = buildPagePaths(coursePlans)
     const { pageSummaries, pdfConversionPaths } = await buildChapterContent(pagePaths)
     const topicLabels = await getTopicLabels(pagePaths)
     const courses = resolveCourses(coursePlans, pageSummaries, topicLabels)
     await generateCourseSQLScript("courses.sql", courses)
-    await Promise.all(
-        pdfConversionPaths.map(({ source, target, isSolution }) =>
-            compilePdfToPublic(source, target, isSolution)
+    if (!skipPdf) {
+        await Promise.all(
+            pdfConversionPaths.map(({ source, target, isSolution }) =>
+                compilePdfToPublic(source, target, isSolution)
+            )
         )
-    )
+    } else {
+        console.log("[builder] Skipping PDF compilation (NODE_ENV=production)")
+    }
     console.log("[builder] SUCCESS")
 }
